7407019dc37afbfc3c56412dcabbe3d6db676a7c
Common <204951789+Commonwealthrocks@users.noreply.github.com>
2026-01-19 08:25:38 +0100

    cm.c; removed temp comments and improved robustness

--- src/c_header_files/cm.h
+++ src/c_header_files/cm.h
@@ -1,12 +1,11 @@
 // cm.h
-// last updated: 18/01/2026 <d/m/y>
+// last updated: 19/01/2026 <d/m/y>
 #ifndef CM_H
 #define CM_H
 #include "../c_header_files/common.h"
 int install_context_menu();
 int uninstall_context_menu();
 int add_to_path();
-
 #endif
 
 // end
