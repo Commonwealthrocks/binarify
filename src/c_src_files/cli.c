bea2cfc12d589a0e1f066662ef7e9236b7b87f94
Common <204951789+Commonwealthrocks@users.noreply.github.com>
2026-01-19 08:26:09 +0100

    cli.c; update CLI handling

--- src/c_src_files/cli.c
+++ src/c_src_files/cli.c
@@ -1,17 +1,18 @@
 // cli.c
-// last updated: 18/01/2026 <d/m/y>
+// last updated: 19/01/2026 <d/m/y>
 #include "../c_header_files/cli.h"
 void print_usage()
 {
-    printf("Binf tool\n\n");
+    printf("binf tool\n\n");
     printf("USAGE:\n");
+    printf("  binf                                                   run the GUI for the app");
     printf("  binf <input.exe> <output.bxe> [-p password] [-silent]  encrypt n' binaryify\n");
     printf("  binf <input.bxe> [-silent]                             decrypt n' run in RAM\n");
     printf("  binf install cm                                        install context menu\n");
     printf("  binf uninstall cm                                      uninstall context menu\n");
     printf("  binf path                                              add to system PATH\n\n");
     printf("OPTIONS:\n");
-    printf("  -p <password>   use custom password (AES-256-GCM + Argon2id)\n");
+    printf("  -p <password>   use custom password (AES-256-GCM + Argon2ID)\n");
     printf("                  if omitted, uses default password\n");
     printf("  -silent         hide all output except from executed program\n\n");
     printf("EXAMPLES:\n");
