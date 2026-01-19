91beb5ae9b8202cc526b14d8e88fe0a2a16921e7
Common <204951789+Commonwealthrocks@users.noreply.github.com>
2026-01-19 08:21:17 +0100

    _compiler_based.c; add compiler checks and system arc checks

--- src/c_src_files/_compiler_based.c
+++ src/c_src_files/_compiler_based.c
@@ -1,7 +1,16 @@
 // _compiler_based.c
-// last updated: 18/01/2026 <d/m/y>
+// last updated: 19/01/2026 <d/m/y>
 #include "../c_header_files/_compiler_based.h"
 #include <windows.h>
+#include <stdio.h>
+
+#if defined(_M_ARM) || defined(_M_ARM64)
+#error "Architecture check failed: ARM / ARM64 is not supported."
+#endif
+
+#if !defined(__GNUC__) && !defined(_MSC_VER)
+#error "Compiler check failed: unknown compiler, please use GCC."
+#endif
 void secure_zero(void *ptr, size_t len)
 {
     RtlSecureZeroMemory(ptr, len);
