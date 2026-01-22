f16707b9527142902fb0ac311a9f316b10f5bc3f
Common <204951789+Commonwealthrocks@users.noreply.github.com>
2026-01-22 09:47:43 +0100

    gui.h; add argon2id params to header file

--- src/c_header_files/gui.h
+++ src/c_header_files/gui.h
@@ -1,7 +1,13 @@
 // gui.h
-// last updated: 19/01/2026 <d/m/y>
+// last updated: 22/01/2026 <d/m/y>
 #ifndef GUI_H
 #define GUI_H
+typedef struct
+{
+    int time_cost;
+    int memory_cost;
+    int parallelism;
+} CryptoParams;
 void c_gui_run();
 #endif
 
